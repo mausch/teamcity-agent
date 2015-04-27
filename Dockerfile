b5d882cbbabe075ce70032a388685327941ebb09
Mauricio Scheffer <mauricioscheffer@gmail.com>
2015-04-27 16:50:52 +0100

    initial

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM java:8
+
+RUN echo deb http://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list && \
+  apt-key adv --keyserver pgp.mit.edu --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9 && \
+  apt-get update && \
+  apt-get install -y sudo wget ca-certificates lxc-docker-1.6.0
+
+# copied from https://bitbucket.org/ariya/docker-centos/src/a4348d8b6aeca6ea2e93d370ab9d80cb2b19f4ca/centos7-teamcity-agent/Dockerfile?at=master
+
+COPY setup-agent.sh /setup-agent.sh
+
+EXPOSE 9090
+CMD sudo -s -- sh -c "TEAMCITY_SERVER=$TEAMCITY_SERVER bash /setup-agent.sh run"
---
+++ setup-agent.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# copied from https://bitbucket.org/ariya/docker-centos/raw/a4348d8b6aeca6ea2e93d370ab9d80cb2b19f4ca/centos7-teamcity-agent/setup-agent.sh
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+AGENT_DIR="${HOME}/agent"
+
+if [ -z "$TEAMCITY_SERVER" ]; then
+    echo "Fatal error: TEAMCITY_SERVER is not set."
+    echo "Launch this container with -e TEAMCITY_SERVER=http://servername:port."
+    echo
+    exit
+fi
+
+if [ ! -d "$AGENT_DIR" ]; then
+    cd ${HOME}
+    echo "Setting up TeamCityagent for the first time..."
+    echo "Agent will be installed to ${AGENT_DIR}."
+    mkdir -p $AGENT_DIR
+    wget $TEAMCITY_SERVER/update/buildAgent.zip
+    unzip -q -d $AGENT_DIR buildAgent.zip
+    rm buildAgent.zip
+    chmod +x $AGENT_DIR/bin/agent.sh
+    echo "serverUrl=${TEAMCITY_SERVER}" > $AGENT_DIR/conf/buildAgent.properties
+    echo "name=" >> $AGENT_DIR/conf/buildAgent.properties
+    echo "workDir=../work" >> $AGENT_DIR/conf/buildAgent.properties
+    echo "tempDir=../temp" >> $AGENT_DIR/conf/buildAgent.properties
+    echo "systemDir=../system" >> $AGENT_DIR/conf/buildAgent.properties
+else
+    echo "Using agent at ${AGENT_DIR}."
+fi
+$AGENT_DIR/bin/agent.sh run
